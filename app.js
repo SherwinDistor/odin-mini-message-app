665433d771c44b2f2adc8a78ac5b6ed00641e67d
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-02-25 16:28:21 -0600

    Create server and routing templates

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set up views folder for ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set up server to listen on port 5000
+app.listen(5000);
+
+// Sample messages
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
+// Routing
+app.get('/', (req, res) => {});
+
+app.get('/new', (req, res) => {});
