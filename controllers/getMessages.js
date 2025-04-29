4a51e3e1f860ec45b9b3a8ecea24453f173fbddc
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-04-29 16:32:25 -0500

    Implement getAllMessages from database and display on page

--- controllers/getMessages.js
+++ controllers/getMessages.js
@@ -1,7 +1,15 @@
-const messages = require('../messages');
+// const messages = require('../messages');
+const db = require('../db/queries');
 
-const getMessages = (req, res) => {
-	res.render('index', { title: 'Mini Messageboard', messages: messages });
-};
+// const getMessages = (req, res) => {
+// 	res.render('index', { title: 'Mini Messageboard', messages: messages });
+// };
+
+async function getMessages(req, res) {
+	const messages = await db.getAllMessages();
+	console.log(messages);
+	// res.send(messages);
+	res.render('index', { title: 'Mini Message App', messages: messages });
+}
 
 module.exports = getMessages;
